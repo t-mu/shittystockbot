b846835e3f44d55e8c61cf53287cdbe384f3c16e
kollimies <teemu.mantyharju@gmail.com>
2015-09-30 22:06:28 +0300

    yolo

---
+++ data/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+		'consumer_key' : 		'super_secret_key',
+		'consumer_secret' : 		'super_secret_secret',
+		'access_token' : 		'super_secret_token',
+		'access_token_secret' : 	'super_secret_token_secret' 
+	
+}
+		
+	
+	
+
